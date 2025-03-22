73ba91523562cb3c65bf416e0de9b394f51a9c4a
Sohil Khan <96345871+sohilcodes@users.noreply.github.com>
2025-03-22 16:11:48 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,34 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet } from 'react-native';
+import { SafeAreaProvider } from "react-native-safe-area-context"
+import { Toaster } from 'sonner-native';
+import HomeScreen from "./screens/HomeScreen"
+import LoginScreen from "./screens/LoginScreen"
+  
+  const Stack = createNativeStackNavigator();
+  
+  function RootStack() {
+    return (      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    );
+  }
+  
+  export default function App() {
+    return (
+      <SafeAreaProvider style={styles.container}>
+      <Toaster />
+        <NavigationContainer>
+          <RootStack />
+        </NavigationContainer>
+      </SafeAreaProvider>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1
+    }
+  });
